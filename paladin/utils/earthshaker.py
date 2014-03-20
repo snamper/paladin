8546a0bfe9708d2986a207cb05c05831ba984deb
stdrickforce <stdrickforce@gmail.com>
2014-03-20 15:02:15 +0800

    earthshaker

---
+++ paladin/utils/earthshaker.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class order_service():
+    pass
+
+
+class user_service():
+    pass
+
+
+class restaurant_service():
+    pass
+
+
+class earthshaker():
+    order = order_service()
+    user = user_service()
+    restaurant = restaurant_service()
+
+    def __init__(self):
+        pass
